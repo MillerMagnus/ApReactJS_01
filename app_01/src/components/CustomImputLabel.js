8d75d2750d01bfe9b0c01884bdf0e72e2a315630
MillerMagnus <millermagnus@outlook.com>
2024-02-19 22:19:52 -0300

    Primeiro Componente Customizado

---
+++ app_01/src/components/CustomImputLabel.js
@@ -0,0 +1,9 @@
+export default function CustomImputLabel(props) {
+    return (
+        <div className="col-8 mb-3">
+        <label className="form-label" for={props.id}>{props.texto}</label>
+        <input className="form-control" id={props.id} type="text" />
+      </div>
+    );
+  }
+  
